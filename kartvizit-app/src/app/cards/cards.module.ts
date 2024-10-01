93372bf612369b061347d09c66a75fff5ac416c5
sbrycbc <sabriyecbc@gmail.com>
2024-10-01 17:24:33 +0200

    added card component with styling...

--- kartvizit-app/src/app/cards/cards.module.ts
+++ kartvizit-app/src/app/cards/cards.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CardsComponent } from './cards.component';
   ],
   imports: [
     CommonModule,
-    CardsRoutingModule
+    CardsRoutingModule,
+    MatCardModule
   ]
 })
 export class CardsModule { }
