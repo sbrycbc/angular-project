625570d878f4e52f183d768b2aeaeb07d76b02dd
sbrycbc <sabriyecbc@gmail.com>
2024-10-01 10:06:59 +0200

    added home, header, about components...

--- kartvizit-app/src/app/app-routing.module.ts
+++ kartvizit-app/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutingModule { }
+
+ export class AppRoutingModule { }
--- kartvizit-app/src/app/app.module.ts
+++ kartvizit-app/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
  
   ],
   imports: [
-    BrowserModule,
+   BrowserModule,
    AppRoutingModule
   ],
   providers: [],
