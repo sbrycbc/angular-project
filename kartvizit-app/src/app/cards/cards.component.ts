3b58a421dedc73fc3515442ab4fcb1b8394ad42e
sbrycbc <sabriyecbc@gmail.com>
2024-10-02 11:48:38 +0200

    added card-item component...

--- kartvizit-app/src/app/cards/cards.component.ts
+++ kartvizit-app/src/app/cards/cards.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrl: './cards.component.scss'
+  styleUrls:[ './cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
 
+  cardItem={
+    title: 'Developer',
+    name: 'SbryCbc',
+    phone: '55555555',
+    email: 'sbrycbc@gmail.com',
+    address: '123 Main St'
+  };
+  
+  constructor() { }
+  ngOnInit(): void { }
 }
+
+
