fb1fbad395123164f7a73b9d71e0ce0c385eff46
sbrycbc <sabriyecbc@gmail.com>
2024-10-14 10:38:24 +0200

    added openUpdateCardModal...

--- kartvizit-app/src/app/cards/card-item/card-item.component.ts
+++ kartvizit-app/src/app/cards/card-item/card-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../../models/card';
+import { MatDialog } from '@angular/material/dialog';
+import { CardModelComponent } from '../card-model/card-model.component';
 
 @Component({
   selector: 'app-card-item',
@@ -10,9 +12,21 @@ export class CardItemComponent implements OnInit {
 
   @Input() cardItem!: Card;
   
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openUpdateCardModal(card: Card): void {
+    this.dialog.open(CardModelComponent, {
+      width: '400px',
+      data: card
+    })
+
+  }
+
 }
+
+
